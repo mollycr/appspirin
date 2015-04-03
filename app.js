42f6bdecf173b61576f5a53327d32765799f2588
Molly Robison <molly.robison@gmail.com>
2015-04-03 17:04:26 +0000

    got rid of templates, also screwed up the repo so hopefully this works

--- app.js
+++ app.js
@@ -1,60 +1,110 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', function(req, res){
+    //show splash screen for n seconds
+    //redirect to login page
+    res.sendFile('templates/home.html', { root: __dirname });
+})
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/u/:user',function(req, res){
+    //show splash screen for n seconds
+    var user = req.params.user;
+    //log in as :user
+    //  if :user doesn't exist, redirect to login
+    //redirect to home
+}); 
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/login', function(req, res){
+    //login or create account - just render
+    res.sendFile('templates/login.html', { root: __dirname });
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.post('/login', function(req, res){
+    //is it a login or a create?
+    //if it's a create, make a new user object, new email in the cron job and redirect to home
+    //if it's a login, load the user appropriately (probably sessions) and redirect to home
 });
 
+app.get('/home', function(req, res){
+    //just render the home page
+    //links to logout, settings, checkin, history, resources
+    res.sendFile('templates/home.html', { root: __dirname });
+});
 
-module.exports = app;
+app.get('/settings', function(req, res){
+    //render settings page
+    //a lot of stuff will post here
+    res.sendFile('templates/settings.html', { root: __dirname });
+});
+
+app.post('/settings', function(req, res){
+    //change the JSON objects appropriately
+});
+
+app.get('/logout', function(req, res){
+    //logout
+    //redirect to login
+    res.sendFile('templates/login.html', { root: __dirname });
+});
+
+app.get('/history', function(req, res){
+    //render the appropriate page
+    res.sendFile('templates/history.html', { root: __dirname });
+});
+
+app.get('/resources', function(req, res){
+    //just render the page
+    res.sendFile('templates/resources.html', { root: __dirname });
+});
+
+app.get('/checkin', function(req, res){
+    //record timestamp and pre-level
+    //send on to suggestions
+    res.sendFile('templates/checkin.html', { root: __dirname });
+});
+
+app.get('/suggestions', function(req, res){
+    //display suggestions for user and level, ordered by usage
+    //send on to strategy page
+    res.sendFile('templates/suggestions.html', { root: __dirname });
+});
+
+app.get('/strat/:strategy', function(req, res){
+    var strategy = req.params.strategy;
+    //render strategy
+    //send on to post checkin
+    res.sendFile('templates/strategy.html', { root: __dirname });
+});
+
+app.get('/checkinPost', function(req, res){
+    //record post score
+    //send to stratEnd
+    res.sendFile('templates/checkinPost.html', { root: __dirname });
+});
+
+app.get('/stratEnd', function(req, res){
+    //render appropriate end message for score difference
+    //send to home
+    res.sendFile('templates/stratEnd.html', { root: __dirname });
+});
+
+app.get('/newStrategy', function(req, res){
+	//render the page, 
+	//then send back to settings if they came from settings 
+	//or the checkin flow if they came from checkin
+	res.sendFile('templates/newStrategy.html', { root: __dirname });
+});
+
+app.post('/newStrategy', function(req, res){
+	//put the info in the right places
+});
+
+var server = app.listen(3000, function(){
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Appspirin listening at http://%s:%s', host, port)
+
+});
